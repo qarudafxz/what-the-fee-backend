69945e6287d4e3242ee4b20be16c32d3dcd9cb6d
qaruda <70809588+francistinao@users.noreply.github.com>
2023-09-21 09:40:06 +0800

    making migrations

---
+++ database/migrations/2023_09_21_011530_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id');
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('program');
+            $table->string('year');
+            $table->decimal('balance', 10, 2);
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
